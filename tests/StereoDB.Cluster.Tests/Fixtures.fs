8b0af8fbde77b611f0a44caa2573e3e22e065849
Oleksii Kraievyi <oleksiikraieviy@gmail.com>
2025-01-04 10:55:08 +0200

    adds kafka fixtures to tests

---
+++ tests/StereoDB.Cluster.Tests/Fixtures.fs
@@ -0,0 +1,24 @@
+﻿module Fixtures
+
+open System
+
+open Confluent.Kafka
+
+open StereoDB.Cluster
+
+module KafkaFixtures =
+    type KafkaConsumerFixture (kafkaSettings: KafkaSettings) =
+        let kafkaConsumer = ConsumerBuilder<string, string>(
+            ConsumerConfig(
+                BootstrapServers = kafkaSettings.KafkaBootstrapServers,
+                AutoOffsetReset = AutoOffsetReset.Latest,
+                GroupId = Random.Shared.NextInt64().ToString())).Build()
+
+        member _.Consumer = kafkaConsumer
+
+        interface IDisposable with
+            member _.Dispose (): unit = 
+                kafkaConsumer.Close()
+                kafkaConsumer.Dispose()
+
+
--- tests/StereoDB.Cluster.Tests/KafkaTests.fs
+++ tests/StereoDB.Cluster.Tests/KafkaTests.fs
@@ -4,50 +4,44 @@ open System
 open System.Collections.Concurrent
 open System.Threading
 open System.Threading.Tasks
-open Confluent.Kafka
 
 open Xunit
 open Swensen.Unquote
 
 open StereoDB.Cluster
+open Fixtures.KafkaFixtures
 
-[<Fact>]
-let ``JoinCluster should trigger sending heartbeat`` () = task {
+type HeartbeatTests(kafkaConsumerFixture: KafkaConsumerFixture) =
+    interface IClassFixture<KafkaConsumerFixture>
 
-    let settings = ClusterSettings.Default
-    let kafkaSettings = settings.KafkaSettings
-    use node = StereoDbNode.Create(settings)
-    node.JoinCluster()
-    
-    use kafkaConsumer = ConsumerBuilder<string, string>(
-        ConsumerConfig(
-            BootstrapServers = kafkaSettings.KafkaBootstrapServers,
-            AutoOffsetReset = AutoOffsetReset.Latest,
-            GroupId = Random.Shared.NextInt64().ToString())).Build()
+    [<Fact>]
+    member _.``JoinCluster should trigger sending heartbeat`` () = task {
+        let settings = ClusterSettings.Default
+        let kafkaSettings = settings.KafkaSettings
+        use node = StereoDbNode.Create(settings)
+        node.JoinCluster()
 
-    use cts = new CancellationTokenSource()
-    cts.CancelAfter(TimeSpan.FromSeconds 5) // wait for heartbeat messages to be sent
+        use cts = new CancellationTokenSource()
+        cts.CancelAfter(TimeSpan.FromSeconds 5) // wait for heartbeat messages to be sent
 
-    let tcs = TaskCompletionSource()
-    let heartbeatMessages = ConcurrentBag<string>()
+        let tcs = TaskCompletionSource()
+        let heartbeatMessages = ConcurrentBag<string>()
 
-    task {
-        let heartbeatTopic =
-            Kafka.createHeartbeatTopicName kafkaSettings.KafkaHeartbeatTopicPrefix settings.ClusterId
+        task {
+            let heartbeatTopic =
+                Kafka.createHeartbeatTopicName kafkaSettings.KafkaHeartbeatTopicPrefix settings.ClusterId
             
-        kafkaConsumer.Subscribe heartbeatTopic
+            kafkaConsumerFixture.Consumer.Subscribe heartbeatTopic
         
-        try
-            while not cts.IsCancellationRequested do
-                let msg = kafkaConsumer.Consume(cts.Token)
-                heartbeatMessages.Add(msg.Message.Value)
-        with
-            _ ->
-                kafkaConsumer.Close()
-                tcs.SetResult()                
-    } |> ignore
+            try
+                while not cts.IsCancellationRequested do
+                    let msg = kafkaConsumerFixture.Consumer.Consume cts.Token
+                    heartbeatMessages.Add(msg.Message.Value)
+            with
+                _ -> tcs.SetResult()                
+        } |> ignore
 
-    do! tcs.Task
+        do! tcs.Task
 
-    test <@ heartbeatMessages.Count > 0 @>
-}
+        test <@ heartbeatMessages.Count > 0 @>
+    }
